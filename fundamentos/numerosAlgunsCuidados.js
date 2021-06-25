f680dd62fe6d9eb47186c7e8f128780b4b41848a
J3eff <j.brandao@expenseon.com>
2021-06-25 00:17:47 -0300

    Number: Alguns Cuidados

---
+++ fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,6 @@
+console.log(7 / 0) // Retorna um Valor Infinito
+console.log("10" / 2) // Ele valida se é possivel fazer a conversão da string da para valor numerico e faz o caculo
+console.log("Show!" * 2) // Retorna Not-A-Number (não é um número)
+console.log(0.1 + 0.7) // Gera uma empresição
+//console.log(10.ToString()) Não é possivel fazer conversão de um numero para string direto
+console.log((10.345).toFixed(2)) // é possivel chamar o literal com o valor dentro de parenteses
