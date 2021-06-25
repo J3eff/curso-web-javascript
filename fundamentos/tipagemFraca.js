ee7f05d66082e0459674565046d6f408c4116b17
J3eff <j.brandao@expenseon.com>
2021-06-24 23:19:31 -0300

    Tipagem fraca

---
+++ fundamentos/tipagemFraca.js
@@ -0,0 +1,14 @@
+let qualquer = 'Legal'; // aspas simples '' e aspas duplas "" referece a uma string 
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = 3.1516
+console.log(qualquer)
+console.log(typeof qualquer)
+
+// Evite nome genéricos e siglas 
+let valor = ''
+let numero = 1
+let pqp = false // Produto Químico Perigoso... kkk 
+
+// escolas de nomes para variaveis são fundamentais 
