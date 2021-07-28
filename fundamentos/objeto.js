7ffd74ee99ffb443ccd72331ad89df5ddd7dab11
J3eff <jeffersonvideo125@gmail.com>
2021-07-27 23:52:24 -0300

    Tipos em JavaScript: Object

---
+++ fundamentos/objeto.js
@@ -0,0 +1,18 @@
+const prod1 = {} // Representa um objeto - e a forma literal de escrever um objeto
+prod1.nome = 'Celular Ultra Mega' // Criado dinamicamento dentro do objeto -  O que é OBJ? - Em JavaScript e uma coleção de Chaves e Valores
+prod1.preco = 4998.90
+prod1['Desconto Legal'] = 0.40  // Evite atributos com espaço
+
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Camisa Polo',
+    preco: 79.90    
+}
+
+// JSON é um formato TEXTUAL |  JSON - JAVASCRIPT OBJECT NOTACION 
+
+// OBJETO é uma coleção de Chave e Valor
+
+console.log(prod2)
+
