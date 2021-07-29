ae738f87aceb719d4e2978ed27a1fcf80cf62060
J3eff <jeffersonvideo125@gmail.com>
2021-07-29 08:48:55 -0300

    Usando Let em loop 01

---
+++ fundamentos/usandoLetEmLoop1.js
@@ -0,0 +1,5 @@
+for(let i = 0; i < 10; i++){
+    console.log(i)
+}
+// Variavel I -> A mesma tem escopo de bloco por ser um LET então não sera possivel imprimir o valor de I fora do bloco
+console.log('i = ', i) 
