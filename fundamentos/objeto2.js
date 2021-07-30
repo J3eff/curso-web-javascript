5dde5a6d3ceb01eb85f3497e4104367057bfe297
J3eff <jeffersonvideo125@gmail.com>
2021-07-29 23:24:11 -0300

    Função Vs Objeto

---
+++ fundamentos/objeto2.js
@@ -0,0 +1,13 @@
+console.log(typeof Object)
+console.log(typeof new Object) // Objeto criado apartir de uma função
+
+const Cliente = function() {}
+console.log(typeof Cliente)
+console.log(typeof new Cliente)
+
+class Produto {} // ES 2015 (ES6) 
+console.log(typeof Produto)
+console.log(typeof new Produto) // Instancia um Objeto da classe
+// Função acaba tendo papel de um classe de definir o molde da estrutura.
+
+// Função construtora? 
