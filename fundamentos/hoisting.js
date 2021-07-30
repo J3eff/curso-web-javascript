f3e3d8411e16451c08e6db2806708b4c20b3261a
J3eff <jeffersonvideo125@gmail.com>
2021-07-29 23:13:49 -0300

    Entendendo o Hoisting

---
+++ fundamentos/hoisting.js
@@ -0,0 +1,9 @@
+// Hoisting - Içamento 
+// Variaveis declaradas com VAR sofre esse efeito
+// Até mesmo dentro de uma função ela sofre o içamento
+console.log('a =', a)
+var a = 2
+
+console.log('a =', a)
+
+console.log()
