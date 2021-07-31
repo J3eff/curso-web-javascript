206ab351021431d6ee8ae19b1ee232f712151145
J3eff <jeffersonvideo125@gmail.com>
2021-07-31 15:12:56 -0300

    Operadores: Aritméticos

---
+++ fundamentos/aritimeticos.js
@@ -0,0 +1,15 @@
+const [a, b, c, d] = [3, 5, 1, 15]
+
+const soma = a+ b + c + d 
+const subtracao = d - b
+const multiplicacao = a * b
+const divisao = d / a 
+const modulo = a % 2 // PAR = 0 IMPAR = 1 - Resta da divisão 
+
+console.log(soma, subtracao, multiplicacao, divisao, modulo)
+
+
+//d++  Operador postfix 
+//++d  Operador prefix
+//op1 + op2  Operando infix
+//-a Operador unario -> Que opera encima de um unico operando
