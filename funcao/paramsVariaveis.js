8052a49ac77665e34a47fb6019e7895a8ddd0e4d
J3eff <jeffersonvideo125@gmail.com>
2021-08-10 08:34:53 -0300

    Parâmetros Variáveis

---
+++ funcao/paramsVariaveis.js
@@ -0,0 +1,15 @@
+// Arguments é um array interno de uma função que tem todos os argumentos que foram passados.
+
+function soma(){
+    let soma = 0
+    for(i in arguments){
+        soma += arguments[i]
+    }
+    return soma
+}
+
+console.log(soma())
+console.log(soma(1))
+console.log(soma(1.1, 2.2, 3.3))
+console.log(soma(1.1, 2.2, "Teste"))
+console.log(soma('a', 'b', 'c'))
