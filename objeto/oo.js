359b1cf704ad30e1bb7fe481f3667cac9dba76d8
J3eff <jeffersonvideo125@gmail.com>
2021-08-24 08:59:16 -0300

    Introdução a OO - Parte 1

---
+++ objeto/oo.js
@@ -0,0 +1,20 @@
+// CÓDIGO NÃO EXECUTAVEL !!! - CONCEITOS DA OO
+
+// Paradigma procedural -> é baseado no procedimento/procedure/função
+
+// Procedural  
+processamento(valor1, valor2, valor3)
+
+// OO
+object = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        // ...
+    }
+}
+
+object.processamento() // Foco passou a ser o objeto.
+
+// 
