2baf3532dd31eab40e0f23907b83f6d4404e62bd
J3eff <j.brandao@expenseon.com>
2021-08-24 22:57:48 -0300

    Introdução a OO

--- objeto/oo.js
+++ objeto/oo.js
@@ -14,7 +14,11 @@ object = {
         // ...
     }
 }
-
 object.processamento() // Foco passou a ser o objeto.
 
-// 
+// Principios importantes:
+// 1. abstração -> abstrair algo do mundo real.
+// 2. encapsulamento -> e você ter os detalhes de implementação escondido e mostrar só para quem precisa usar.
+// 3. herança (protype) -> não aceita heranças multiplas 
+// 4. polimorfismo -> multiplas forma
+ 
