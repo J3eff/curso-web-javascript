ae3e84c43339828e3b47be696a7b6e8706461d2a
J3eff <j.brandao@expenseon.com>
2021-08-25 00:04:24 -0300

    Exercicio 01 - Funcao

---
+++ funcao/exercicios/ex01.js
@@ -0,0 +1,10 @@
+function calculo(a = 1, b = 1){
+    return console.log(`
+    Soma : ${a + b}, 
+    Subtração: ${a - b}, 
+    Multiplicação: ${a * b}, 
+    Divisão: ${a / b}`)
+}
+
+calculo()
+calculo(20,10)
