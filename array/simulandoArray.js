d327f8a7b8331928c03213eb6f19e6b27e1328f8
J3eff <jeffersonvideo125@gmail.com>
2021-10-08 08:39:54 -0300

    Foreach #01

--- array/simulandoArray.js
+++ array/simulandoArray.js
@@ -4,8 +4,8 @@ Object.defineProperty(quaseArray, 'toString', {
     value: function() { return Object.values(this)},
     enumerable: false,
 })
-console.log(quaseArray[0])
+console.log(quaseArray[0]);
 
 
 const meuArray = ['Rafael', 'Ana', 'Bia']
-console.log(quaseArray.toString(), meuArray)
+console.log(quaseArray.toString(), meuArray);
